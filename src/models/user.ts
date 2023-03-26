a6cf876235e5ba1bf9b08d245e5722248f114235
oscarboullosa <oscar.boullosa@estudiantat.upc.edu>
2023-03-26 12:52:28 +0200

    Ejercicio JWT

---
+++ src/models/user.ts
@@ -0,0 +1,40 @@
+import {  Schema, Types, model, Model } from "mongoose";
+import { User } from "../interfaces/user.interface";
+
+const UserSchema = new Schema<User>(
+    {
+        name:{
+            type: String,
+            required:true,
+        },
+        surname:{
+            type: String,
+            required:true,
+        },
+        email:{
+            type: String,
+            required:true,
+            unique: true,
+        },
+        password:{
+            type: String,
+            required:true,
+        },
+        role:{
+            type: String,
+            enum: ["basic", "admin"],
+            required:false,
+        }
+    },
+    {
+        timestamps: true,
+        versionKey: false,
+    }
+);
+
+//Once the Schema is created, it must be implemented
+//1st argument ('users') is the name of the collection
+//2nd argument (UserSchema) is what it feds it
+const UserModel = model('users', UserSchema);
+
+export default UserModel;
